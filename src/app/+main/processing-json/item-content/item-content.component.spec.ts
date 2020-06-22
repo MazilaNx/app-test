7dbb1f280fe0623e8f306b39a80653a5f526287f
Sergey <mazilanx@gmail.com>
2020-06-22 06:24:51 +0300

    minors fix

--- src/app/+main/processing-json/item-content/item-content.component.spec.ts
+++ src/app/+main/processing-json/item-content/item-content.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ItemsComponent } from './item-content.component';
+import { ItemContentComponent } from './item-content.component';
 
-describe('ItemsComponent', () => {
-  let component: ItemsComponent;
-  let fixture: ComponentFixture<ItemsComponent>;
+describe('ItemContentComponent', () => {
+  let component: ItemContentComponent;
+  let fixture: ComponentFixture<ItemContentComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ItemsComponent ]
+      declarations: [ ItemContentComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ItemsComponent);
+    fixture = TestBed.createComponent(ItemContentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
